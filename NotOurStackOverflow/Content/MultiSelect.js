d0baaeda4b07fdbf38ab0b53d51378c03cf13cf3
Julien Labossiere <jdhlabossiere@gmail.com>
2019-11-07 14:34:40 -0600

    added multiSelectList code

---
+++ NotOurStackOverflow/Content/MultiSelect.js
@@ -0,0 +1,19 @@
+﻿let optionList = document.querySelectorAll("option")
+
+optionList.forEach(option => {
+    option.addEventListener("mousedown", e => {
+        e.preventDefault();
+        let target = e.target;
+        if (target.getAttribute("selected") == null || target.getAttribute("selected") === false) {
+            target.setAttribute("selected", true)
+        } else {
+            target.removeAttribute("selected")
+        }
+        return;
+    })
+})
+//csHtml for MultiSelectList
+//   @Html.ListBox("values", (MultiSelectList)ViewBag.ListTest)
+
+//Controller C#  note: 
+//   ViewBag.ListTest = new MultiSelectList(List<T> List of items, (string)ListItemValue, (string)ListItemName);
