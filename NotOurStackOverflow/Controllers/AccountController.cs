7b9a3f886ac7649261b2c61526e50b72b9371896
Jeff Cadorath <JJcadorath@hotmail.com>
2019-11-12 13:31:12 -0600

    -Updated Seed Method to properly allow logins

--- NotOurStackOverflow/Controllers/AccountController.cs
+++ NotOurStackOverflow/Controllers/AccountController.cs
@@ -17,6 +17,7 @@ namespace NotOurStackOverflow.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        ApplicationDbContext db = new ApplicationDbContext();
 
         public AccountController()
         {
@@ -75,7 +76,8 @@ namespace NotOurStackOverflow.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var user = db.Users.Where(x => x.Email == model.Email).FirstOrDefault();
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, true, false);
             switch (result)
             {
                 case SignInStatus.Success:
