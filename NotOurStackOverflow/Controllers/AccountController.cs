99920f04f46512ffc21534e762007166ba4faea7
Zacharie <zemontreuil@outlook.com>
2019-11-21 13:47:09 -0600

    - Redirects the user to the landing pages upon registration

--- NotOurStackOverflow/Controllers/AccountController.cs
+++ NotOurStackOverflow/Controllers/AccountController.cs
@@ -172,7 +172,7 @@ namespace NotOurStackOverflow.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("LandingPage", "Questions");
                 }
                 AddErrors(result);
             }
